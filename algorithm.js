c6fdaa237aa3aa689d0830b33c16d4c6be90c278
Mohamed Abusaif <67015050+Mohamed-Abusaif@users.noreply.github.com>
2024-06-09 19:28:42 +0300

    Finish The Basic Functionality

--- algorithm.js
+++ algorithm.js
@@ -1,17 +1,30 @@
 const collatzConjectureAlgorithm = (int) => {
   let count = 0;
+  let theSecquence = [];
   while (int !== 1) {
     if (int % 2 == 0) {
       int = int / 2;
       count++;
-      console.log(int);
+      theSecquence.push(int);
     } else {
       int = 3 * int + 1;
       count++;
-      console.log(int);
+      theSecquence.push(int);
     }
   }
-  console.log(`The Total Number of steps to reach 1: ${count}`);
+  const output = {
+    countOfSteps: count,
+    sequenceItself: theSecquence,
+  };
+  return output;
 };
 
-collatzConjectureAlgorithm(27);
+function processingInput() {
+  const integerNumber = parseInt(document.getElementById("integer").value);
+  const result = collatzConjectureAlgorithm(integerNumber);
+
+  document.getElementById("result").innerHTML = result.sequenceItself;
+  document.getElementById("count").innerHTML = result.countOfSteps;
+
+  return false;
+}
