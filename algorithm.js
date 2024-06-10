cb53c326ef189d5c37c3b3f9dbf0bbbd0e326c58
Mohamed Abusaif <67015050+Mohamed-Abusaif@users.noreply.github.com>
2024-06-11 02:57:25 +0300

    Update the main form style Add d3.js pre template in visualization.js file to start modifying it for our specific data

--- algorithm.js
+++ algorithm.js
@@ -19,12 +19,39 @@ const collatzConjectureAlgorithm = (int) => {
   return output;
 };
 
+const displayResult = (sequenceItself) => {
+  const numberContainer = document.getElementById("result");
+
+  for (let i = 0; i < sequenceItself.length; i++) {
+    const numberElement = document.createElement("span");
+    numberElement.className = "number";
+    numberElement.textContent = sequenceItself[i];
+    numberContainer.appendChild(numberElement);
+
+    if (i < sequenceItself.length - 1) {
+      const separator = document.createElement("span");
+      separator.textContent = i % 10 === 9 ? "" : ", ";
+      numberContainer.appendChild(separator);
+    }
+
+    if ((i + 1) % 10 === 0) {
+      const brElement = document.createElement("br");
+      numberContainer.appendChild(brElement);
+    }
+  }
+};
+
 const returnOutput = () => {
   const integerNumber = parseInt(document.getElementById("integer").value);
   const result = collatzConjectureAlgorithm(integerNumber);
 
-  document.getElementById("result").innerHTML = result.sequenceItself;
+  document.getElementById("chart").innerHTML = "";
+  document.getElementById("result").innerHTML = "";
+
+  displayResult(result.sequenceItself);
   document.getElementById("count").innerHTML = result.countOfSteps;
+
   visualize(result.sequenceItself);
+
   return false;
 };
--- visualization.js
+++ visualization.js
@@ -1,22 +1,98 @@
 const visualize = (result) => {
-  // Set up SVG canvas
-  var width = 500;
-  var height = 300;
-  var svg = d3
+  // set the dimensions and margins of the graph
+  var margin = { top: 30, right: 30, bottom: 50, left: 60 },
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+  // append the svg object to the div container
+  var Svg = d3
     .select("#chart")
     .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // Create bars
-  svg
-    .selectAll("rect")
+  // Add border
+  Svg.append("rect")
+    .attr("x", -margin.left)
+    .attr("y", -margin.top)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .style("stroke", "#0d6efd")
+    .style("fill", "none")
+    .style("stroke-width", "5px");
+
+  // Add X axis
+  var x = d3
+    .scaleLinear()
+    .domain([4 * 0.95, 8 * 1.001])
+    .range([0, width]);
+  Svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(
+      d3
+        .axisBottom(x)
+        .tickSize(-height * 1.3)
+        .ticks(10)
+    )
+    .select(".domain")
+    .remove();
+
+  // Add Y axis
+  var y = d3
+    .scaleLinear()
+    .domain([-0.001, 9 * 1.01])
+    .range([height, 0])
+    .nice();
+  Svg.append("g")
+    .call(
+      d3
+        .axisLeft(y)
+        .tickSize(-width * 1.3)
+        .ticks(7)
+    )
+    .select(".domain")
+    .remove();
+
+  // Customization
+  Svg.selectAll(".tick line").attr("stroke", "#EBEBEB");
+
+  // Add X axis label:
+  Svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height + margin.top + 20)
+    .text("Sepal Length");
+
+  // Y axis label:
+  Svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left + 20)
+    .attr("x", -margin.top)
+    .text("Petal Length");
+
+  // Color scale: give me a specie name, I return a color
+  var color = d3
+    .scaleOrdinal()
+    .domain(["setosa", "versicolor", "virginica"])
+    .range(["#402D54", "#D18975", "#8FD175"]);
+
+  // Add dots
+  Svg.append("g")
+    .selectAll("dot")
     .data(result)
     .enter()
-    .append("rect")
-    .attr("x", (d, i) => i * 70)
-    .attr("y", (d) => height - d)
-    .attr("width", 65)
-    .attr("height", (d) => d)
-    .attr("fill", "blue");
+    .append("circle")
+    .attr("cx", function (d) {
+      return x(d.Sepal_Length);
+    })
+    .attr("cy", function (d) {
+      return y(d.Petal_Length);
+    })
+    .attr("r", 5)
+    .style("fill", function (d) {
+      return color(d.Species);
+    });
 };
